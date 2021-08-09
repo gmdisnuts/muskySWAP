88d5171752cd2e9bb63823b327296228574a6c1e
Alex <king.zan@hotmail.com>
2021-08-08 22:06:45 -0500

    added token coin swap img

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,6 +8,7 @@ type ChainTokenList = {
 }
 
 export const CAKE = new Token(ChainId.MAINNET, '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', 18, 'CAKE', 'PancakeSwap Token')
+export const RBL = new Token(ChainId.MAINNET, '0xdef92bc56a25597dfdba4a10b453799729080d9a', 18, 'RBL', 'Red Balloon Token')
 export const WBNB = new Token(ChainId.MAINNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB')
 export const DAI = new Token(ChainId.MAINNET, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin')
 export const BUSD = new Token(ChainId.MAINNET, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 'BUSD', 'Binance USD')
@@ -64,6 +65,7 @@ export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } 
     [CAKE, WBNB],
     [BUSD, USDT],
     [DAI, USDT],
+    [RBL, WBNB]
   ],
 }
 
